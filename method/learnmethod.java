7dfd5353a1c1865d5fdb2cce6fc0d6c386b188bd
ashishraj1104 <ashishraj1999745@gmail.com>
2025-02-26 22:06:39 +0530

    problem

---
+++ method/learnmethod.java
@@ -0,0 +1,12 @@
+package method;
+
+public class learnmethod {
+    public static void greet(){
+        System.out.println(5);
+    }
+    
+
+public static void main(String[]args){
+        greet();
+}
+}
